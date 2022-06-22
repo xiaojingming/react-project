dfdb2ce29f48ac13f9a5e9030610d609ad2532bd
xiaojingming <2413294257@qq.com>
2022-06-22 09:08:33 +0800

    tsx test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css';
+// import './App.css';
 
+const list = [
+  {
+    id: 1,
+    name: 'a',
+  },
+  {
+    id: 2,
+    name: 'b',
+  },
+  {
+    id: 3,
+    name: 'c',
+  },
+];
+const flag = false;
 function App() {
   return (
-    <div className="App">
-      react project
-    </div>
+    <>
+      <div className="App">
+        <ul>
+          { list.map((item) => (<li key={item.id}>{ item.name }</li>)) }
+        </ul>
+      </div>
+      <div>{ flag ? 'hello xiao' : 'xjm' }</div>
+    </>
   );
 }
 
