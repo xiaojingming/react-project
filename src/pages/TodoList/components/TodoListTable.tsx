2e9a48831220dba9fba76390ce1de85e2d0b17ca
xiaojingming <2413294257@qq.com>
2022-08-09 10:31:23 +0800

    todolist base style

--- src/pages/TodoList/components/TodoListTable.tsx
+++ src/pages/TodoList/components/TodoListTable.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Table } from 'antd';
+import { Table, Tag, Space } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import './table.scss';
 
 interface DataType {
   key: string;
@@ -38,22 +38,45 @@ const columns: ColumnsType<DataType> = [
     title: 'Name',
     key: 'name',
     dataIndex: 'name',
+    render(value: string) {
+      return (<span>{value}</span>);
+    },
   },
   {
     title: 'Age',
     key: 'nage',
+    dataIndex: 'age',
   },
   {
     title: 'Address',
     key: 'address',
+    dataIndex: 'address',
   },
   {
     title: 'Tags',
-    key: 'tag',
+    key: 'tags',
+    dataIndex: 'tags',
+    render(value: string[]) {
+      return value.map((item) => {
+        let color = item.length > 5 ? 'geekblue' : 'green';
+        if (item === 'loser') {
+          color = 'volcano';
+        }
+        return (<Tag key={item} color={color}>{item.toUpperCase()}</Tag>);
+      });
+    },
   },
   {
     title: 'Action',
     key: 'action`',
+    render(value, { name }: DataType) {
+      return (
+        <Space>
+          <span>{`Invite ${name}`}</span>
+          <span>Delete</span>
+        </Space>
+      );
+    },
   },
 ];
 
