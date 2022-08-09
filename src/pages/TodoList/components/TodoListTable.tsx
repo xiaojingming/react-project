b3d256e49d0eda121641f2f98b1a81fabc4f0457
xiaojingming <2413294257@qq.com>
2022-08-09 22:02:26 +0800

    finish search function

--- src/pages/TodoList/components/TodoListTable.tsx
+++ src/pages/TodoList/components/TodoListTable.tsx
@@ -1,38 +1,8 @@
 import { Table, Tag, Space } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import type { DataType } from '../index';
 import './table.scss';
 
-interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-}
-
-const data: DataType[] = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
 const columns: ColumnsType<DataType> = [
   {
     title: 'Name',
@@ -80,7 +50,8 @@ const columns: ColumnsType<DataType> = [
   },
 ];
 
-function TodoListTable() {
+function TodoListTable(props: { data: DataType[] }) {
+  const { data } = props;
   return (
     <Table dataSource={data} columns={columns} />
   );
