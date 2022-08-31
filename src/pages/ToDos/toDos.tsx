56278176ac0eb5e713a0b650987aab433be65d5a
xiaojingming <2413294257@qq.com>
2022-08-31 10:08:10 +0800

    todos function

--- src/pages/ToDos/toDos.tsx
+++ src/pages/ToDos/toDos.tsx
@@ -1,10 +1,27 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
+import { observer } from 'mobx-react-lite';
+import { KeyboardEvent } from 'react';
 import todos from './style/todo.module.scss';
 import useStore from '../../store';
 
 function ToDos() {
   const { todo } = useStore();
+  const handleCheck = (id: string, status: boolean) => {
+    todo.changeItemStatus(id, !status);
+  };
+  const handleCheckAll = () => {
+    todo.changeStatus(!todo.isCheckAll);
+  };
+  const handleDelItem = (index: number) => {
+    todo.delItem(index);
+  };
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      todo.addItem((e.target as HTMLInputElement).value);
+      (e.target as HTMLInputElement).value = '';
+    }
+  };
   return (
     <section className={todos.todoapp}>
       <header className={todos.header}>
@@ -13,6 +30,7 @@ function ToDos() {
           className={todos['new-todo']}
           autoComplete="off"
           placeholder="What needs to be done?"
+          onKeyDown={handleKeyDown}
         />
       </header>
       <section className={todos.main}>
@@ -20,27 +38,46 @@ function ToDos() {
           id="toggle-all"
           className={todos['toggle-all']}
           type="checkbox"
+          checked={todo.isCheckAll}
+          onChange={handleCheckAll}
         />
         <label htmlFor="toggle-all" />
         <ul className={todos['todo-list']}>
           {
-            todo.dataList.map((item) => (
+            todo.dataList.map((item, index) => (
               <li
-                className={todos.todo}
                 key={item.id}
               >
                 <div className={todos.view}>
-                  <input className={todos.toggle} type="checkbox" />
+                  <input
+                    className={todos.toggle}
+                    type="checkbox"
+                    checked={item.completed}
+                    onChange={() => handleCheck(item.id, item.completed)}
+                  />
                   <label>{item.title}</label>
-                  <button className={todos.destroy} type="button" />
+                  <button
+                    className={todos.destroy}
+                    type="button"
+                    onClick={() => handleDelItem(index)}
+                  />
                 </div>
               </li>
             ))
           }
         </ul>
       </section>
+      <footer className={todos.footer}>
+        <span className={todos['todo-count']}>
+          任务总数:
+          {todo.dataList.length}
+          &nbsp;
+          已完成:
+          {todo.checkList.length}
+        </span>
+      </footer>
     </section>
   );
 }
 
-export default ToDos;
+export default observer(ToDos);
--- src/store/modules/todoStore.ts
+++ src/store/modules/todoStore.ts
@@ -1,9 +1,11 @@
-import { makeAutoObservable } from 'mobx';
+/* eslint-disable no-param-reassign */
+import { makeAutoObservable, computed } from 'mobx';
 import uuid from 'react-uuid';
 
 interface List {
   id: string;
   title: string;
+  completed: boolean;
 }
 
 class TodoStore {
@@ -11,15 +13,55 @@ class TodoStore {
     {
       id: uuid(),
       title: 'JS深入系列',
+      completed: true,
     },
     {
       id: uuid(),
       title: 'JS专题系列',
+      completed: false,
     },
   ];
 
   constructor() {
-    makeAutoObservable(this);
+    makeAutoObservable(this, {
+      checkList: computed,
+      isCheckAll: computed,
+    });
+  }
+
+  get checkList() {
+    return this.dataList.filter((item) => item.completed);
+  }
+
+  get isCheckAll() {
+    return this.checkList.length === this.dataList.length;
+  }
+
+  changeItemStatus(id: string, status: boolean) {
+    this.dataList.forEach((item) => {
+      if (item.id === id) {
+        item.completed = status;
+      }
+    });
+  }
+
+  changeStatus(status: boolean) {
+    this.dataList.forEach((item) => {
+      item.completed = status;
+    });
+  }
+
+  delItem(index: number) {
+    this.dataList.splice(index, 1);
+  }
+
+  addItem(title: string) {
+    const obj = {
+      id: uuid(),
+      title,
+      completed: false,
+    };
+    this.dataList.push(obj);
   }
 }
 
